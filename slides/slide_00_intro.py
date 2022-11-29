45dad2a1204ef1f1db9ee1d30823049eb83eefa1
LaFabricaImaginaria <119337378+LaFabricaImaginaria@users.noreply.github.com>
2022-11-29 12:28:20 -0300

    Update slide_00_intro.py

--- slides/slide_00_intro.py
+++ slides/slide_00_intro.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from datetime import date
+now = date.now()
 
 from code.shared_functions import skip_echo
 
@@ -8,15 +10,16 @@ def display():
     _, c1, c2, _ = st.columns([2,7,3,2])
     with c1:
         repo_path = "https://raw.githubusercontent.com/sebastiandres/talk_2021_11_pyconcl/main/images"
-        st.markdown("## Pycon Chile")
-        st.markdown("# WebApps con Streamlit:")
-        st.markdown("#    _¡más fácil que la tabla del uno, poh!_")
+        
+        
         st.markdown("")
         st.markdown("")
         st.markdown("")
-        st.markdown("## Sebastián Flores, Noviembre 2021")
+        st.markdown("# Reporte Mensual  2022")
+        st.markdown("## Walmart Chile")
+        st.markdown("# Estrategia Digital & RRSS")
         st.markdown("##### Presentación realizada en streamlit (:exploding_head: *streamlit-ception*)")
-        st.write("Repositorio de la presentación: https://github.com/sebastiandres/talk_2021_11_pyconcl")
+        st.write(".format(now.month),.format(now.year)")
 
     with c2:
         st.image("images/streamlit_cl.png")
